0cdaf8ab826df8ddd0c448d1e5a4bdaa95399c38
basile <biblex1222@gmail.com>
2020-11-26 17:18:07 +0100

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+type Props = {
+    content: number
+}
+
+const Square: React.FC<Props> = ({content}) => {
+    return (
+        <button className="square">
+            {content}
+        </button>
+    )
+}
+
+function Board() {
+
+    const renderSquare = (i: number) => {
+        return <Square content={i}/>;
+    }
+
+    const [status, setStatus] = useState("Next player: X")
+    // const status : string  = 'Next player: X'
+
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board/>
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game/>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
